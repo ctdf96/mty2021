2e1757277e5f05af81314ab9ee885fb340ca96d8
ctdf96 <88641642+ctdf96@users.noreply.github.com>
2021-08-20 16:17:46 +0900

    Create text.java

---
+++ text.java
@@ -0,0 +1,25 @@
+int k = 1;
+while(k <= 9) {
+
+int l =1;
+while(l <= 9) {
+	System.out.print(k * l +"");
+	l++;
+	}
+	System.out.println();
+	k++;
+	}
+
+for(int i = 1;1<=100;i++) {
+	if(i==51) {
+		break;
+		}
+
+}
+
+for(int i =1;i<=10;i++) {
+	if(i==5) {
+		continue;
+		}
+	System.out.println("飲み会の案内をする");
+	}
