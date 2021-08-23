b32e559b1da1fea301b3eaff01bfb9272d57c363
ctdf96 <88641642+ctdf96@users.noreply.github.com>
2021-08-23 14:04:56 +0900

    Update ParseSample.java

--- ParseSample.java
+++ ParseSample.java
@@ -5,7 +5,7 @@ public class ParseSample {
 		int score = Integer.parseInt(args[0]);
 		double dscore = Double.parseDouble(args[1]);
 		
-		if (dscore >=80) {
+		if (dscore >=　80) {
 			System.out.println("合格です");
 		} else {
 			System.out.println("不合格です");
